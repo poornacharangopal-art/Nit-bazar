b8c62e9706b4fa2a15ecbaf1d17a824fc3eea5ff
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-12 17:19:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -177,6 +177,12 @@ app.get("/profile",(req,res)=>{
         college: req.session.college
     });
 });
+app.get("/addproduct",(req,res)=>{
+    res.render("AddProduct");
+}
+app.post("/addproductdetails",(req,res)=>{
+    const {productname,cost,image}=req.body;
+    
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
