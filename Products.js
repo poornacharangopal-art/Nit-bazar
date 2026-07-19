12d4e16e0b0ea6795f37a349b88f016517d03d57
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-19 11:40:01 +0530

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const ProductsSchema=new mongoose.Schema({
+  Name:{
+    type:string,
+    required:true
+  },
+  UserEmail:{
+    type:string,
+    required:name
+  },
+  UserName:{
+    type:string,
+    required:true
+  },
+  College:{
+    type:string,
+    required:true
+  }
+})
+module.export=model.mongoose("product",ProductSchema);
+
+  
