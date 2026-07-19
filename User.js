77cf12542b1521326a489ef9e5cf88c70ba666b0
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-19 11:45:56 +0530

    Create User schema for MongoDB with Mongoose

---
+++ User.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+const UserSchema=new mongoose.Schema({
+  EmailId:{
+    type:string,
+    required:true
+  },
+  UserId:{
+    type:string,
+    required:true
+  },
+  UserName:{
+    type:string,
+    required:true
+  },
+  College:{
+    type:string,
+    required:true
+  },
+  password:{
+    type:string,
+    required:true
+  }
+});
+module.export=model.mongoose("Users",UserSchema);
